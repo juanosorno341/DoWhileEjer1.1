23d1c0bbf5ca6c2db8dd2994926c5f0bf9ccbd48
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-03-10 04:13:17 -0500

    Agregar archivos de proyecto.

---
+++ DoWhileEjer1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileEjer1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0,
+            suma = 0,
+            r = 0;
+            do 
+            {
+                Console.WriteLine(x);
+                x++;
+                //if (x == 5)  //en estas dos lineas se ejecuta eun brake cuando llegue al numero indicado en el if (x == 5)
+                  //  break;
+            } while (x <100);
+            r = suma + x;
+            {
+                Console.WriteLine(r);     
+            }
+            Console.ReadKey();  
+
+        }
+    }
+}
